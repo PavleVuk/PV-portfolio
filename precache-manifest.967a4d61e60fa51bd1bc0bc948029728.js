d682b3c85bf64207cac37e590014a7fad3dcd966
pavlev <pavlevuk7@gmail.com>
2020-08-09 15:18:20 +0200

    Updates

---
+++ precache-manifest.967a4d61e60fa51bd1bc0bc948029728.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ba7643e081aec87deb1c5c4cfcf722",
+    "url": "/PV-portfolio/index.html"
+  },
+  {
+    "revision": "f62442977be59fc2781b",
+    "url": "/PV-portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "9af25d1ad7b4a6104553",
+    "url": "/PV-portfolio/static/css/main.22708197.chunk.css"
+  },
+  {
+    "revision": "f62442977be59fc2781b",
+    "url": "/PV-portfolio/static/js/2.5a5e01ce.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/PV-portfolio/static/js/2.5a5e01ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af25d1ad7b4a6104553",
+    "url": "/PV-portfolio/static/js/main.7b3d687c.chunk.js"
+  },
+  {
+    "revision": "f1051bc9cbe69c4432d8",
+    "url": "/PV-portfolio/static/js/runtime-main.cb34ec46.js"
+  },
+  {
+    "revision": "29cd7602f5ffaa65e0a87cf0be468315",
+    "url": "/PV-portfolio/static/media/Brisanje.29cd7602.JPG"
+  },
+  {
+    "revision": "cdad2bf4065a6ce58a3f3f9525ea7bfb",
+    "url": "/PV-portfolio/static/media/Izmena.cdad2bf4.JPG"
+  },
+  {
+    "revision": "c1e49a654ce583924788e2a039e90b9e",
+    "url": "/PV-portfolio/static/media/JSp.c1e49a65.JPG"
+  },
+  {
+    "revision": "c36443537face97beeb2a6a21f65e425",
+    "url": "/PV-portfolio/static/media/Lesshosting.c3644353.png"
+  },
+  {
+    "revision": "9ad07e79bb6de770973f6182519dd1a2",
+    "url": "/PV-portfolio/static/media/Lessindex.9ad07e79.png"
+  },
+  {
+    "revision": "d7d06c10c8a164c7790a9eea09307e8a",
+    "url": "/PV-portfolio/static/media/Lesskasa.d7d06c10.png"
+  },
+  {
+    "revision": "cc03da27db5ff94249ca53fdaff9bb40",
+    "url": "/PV-portfolio/static/media/Lesskontakt.cc03da27.png"
+  },
+  {
+    "revision": "b44eaa894ea090e8ccf855ab231d8393",
+    "url": "/PV-portfolio/static/media/Lessp.b44eaa89.JPG"
+  },
+  {
+    "revision": "6c3049f1c49811be1056ee893a016032",
+    "url": "/PV-portfolio/static/media/Lessservis.6c3049f1.png"
+  },
+  {
+    "revision": "c381ffce64b671100831cd05f481ab21",
+    "url": "/PV-portfolio/static/media/Logout.c381ffce.JPG"
+  },
+  {
+    "revision": "cc90d4ecee702e0112f72c52ff1f5a79",
+    "url": "/PV-portfolio/static/media/Pretraga.cc90d4ec.JPG"
+  },
+  {
+    "revision": "5b4562eeab14a95eeab05634efcc9077",
+    "url": "/PV-portfolio/static/media/Registracija.5b4562ee.JPG"
+  },
+  {
+    "revision": "dbc9ac1ebb893c921f0f675e89c5a4b6",
+    "url": "/PV-portfolio/static/media/Unos.dbc9ac1e.JPG"
+  },
+  {
+    "revision": "0c001bd6388d92a0bc5a89bafe33217d",
+    "url": "/PV-portfolio/static/media/avatar.0c001bd6.png"
+  },
+  {
+    "revision": "3e78e45375edcd6cc7e076bb7ad6a12e",
+    "url": "/PV-portfolio/static/media/bs.3e78e453.JPG"
+  },
+  {
+    "revision": "7260bfbe3ac2de0e8abd5594cd9454bd",
+    "url": "/PV-portfolio/static/media/less.7260bfbe.jpg"
+  },
+  {
+    "revision": "04f3a5bcda316ebf239cfb95f9645a2f",
+    "url": "/PV-portfolio/static/media/php.04f3a5bc.JPG"
+  }
+]);
